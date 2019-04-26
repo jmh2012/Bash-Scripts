8c6985bb464d90f402fba5171654272e853ab8d6
jmh2012 <jesse.hagewood@gmail.com>
2019-04-25 22:42:29 -0400

    dns-diff.sh

---
+++ dns-diff.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FILEDIR=/opt/dns-diff/dns-zones-repo
+LOGFILE=/opt/dns-diff/dns-diff.log
+DOMAINS=/opt/dns-diff/domains.list
+
+while IFS= read -r domain
+do
+  echo "$( date ): Checking $domain for changes." >> $LOGFILE
+  if ! diff <( cat $FILEDIR/$domain | grep -v WHEN | grep -v "Query time" | grep -v SOA ) \
+            <( dig AXFR $domain | grep -v WHEN | grep -v "Query time" | grep -v SOA) >> $LOGFILE; then
+    dig AXFR $domain > $FILEDIR/$domain
+    git add $FILEDIR/*
+    git commit -m "$domain changed on $(date))" >> $LOGFILE
+    git push origin master  >> $LOGFILE
+  fi
+done <"$DOMAINS"
